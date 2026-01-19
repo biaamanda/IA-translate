a38e8ff54b049c6f4be92556a9077c3d03d2101a
Amanda Bialer <amandabialer@gmail.com>
2026-01-19 13:21:38 -0300

    add js

---
+++ scripts.js
@@ -0,0 +1,34 @@
+let typedTexted = document.querySelector(".inputText");
+let translatedText = document.querySelector(".translation");
+let idiome = document.querySelector(".languages");
+
+async function translate() {
+  let url =
+    "https://api.mymemory.translated.net/get?q=" +
+    typedTexted.value +
+    "&langpair=pt|" +
+    idiome.value;
+
+  let response = await fetch(url);
+
+  let data = await response.json();
+
+  translatedText.textContent = data.responseData.translatedText;
+}
+
+function microphone() {
+  let speech = window.webkitSpeechRecognition;
+
+  let recognition = new speech();
+
+  recognition.lang = "pt";
+
+  recognition.onresult = (event) => {
+    let spokenText = event.results[0][0].transcript;
+
+    typedTexted.textContent = spokenText;
+
+    translate();
+  };
+  recognition.start();
+}
